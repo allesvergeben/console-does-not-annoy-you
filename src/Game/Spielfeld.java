92f16c435b04802dc12c8c6ce7f1c516538206fd
laurin-d <laurin.doerre@lgs-hu.eu>
2019-11-09 08:57:02 +0100

    Revert "• added ability to move in winspots"
    This reverts commit bc47aea6

--- src/Game/Spielfeld.java
+++ src/Game/Spielfeld.java
@@ -37,22 +37,35 @@ public class Spielfeld extends Spiel {
         try {
             Feld = p_Wurf + getPlayer(p_Spieler - 1).getFigurByHash(p_FigurHash).getFeld();
         } catch (NullPointerException | IndexOutOfBoundsException e) {
-            //NÖTIG FÜR DEN WINSPOT WALK MERGE
+            //Sollte nicht mehr auftreten…
             return false;
         }
         if (Feld >= 40) {
             Feld = Feld - 39;
             getPlayer(p_Spieler - 1).getFigurByHash(p_FigurHash).setRound(true);
         }
-        return walk(p_Spieler - 1, p_FigurHash, Feld);
-        //vorher checken ob sie nicht in den Winspot kommt
+        if (!gohome(p_Spieler - 1, p_FigurHash, Feld)) {
+            //vorher checken ob sie nicht in den Winspot kommt
+            if (Spielfeld[Feld][0] == p_Spieler) {
+                new PlayerOutput().blockedbyown();
+                return false;
+            } else if (Spielfeld[Feld][0] != p_Spieler && Spielfeld[Feld][0] != 0) {
+
+                //Figur schmeißen
+                int kickout = getPlayer(Spielfeld[Feld][0] - 1).getFigurIndexByHash(Spielfeld[Feld][1]);
+                new PlayerOutput().kicked(getPlayer(p_Spieler - 1).getName(), getPlayer(Spielfeld[Feld][0] - 1).getName(), getPlayer(p_Spieler - 1).getId(), Spielfeld[Feld][0], kickout);
+
+                getPlayer(Spielfeld[Feld][0] - 1).getFigures().remove(kickout); //GEWORFEN!
+                switchFig(p_Spieler - 1, p_FigurHash, Feld);
+            } else {
+                switchFig(p_Spieler - 1, p_FigurHash, Feld);
+            }
+        }
+        return true;
     }
 
     private static void switchFig(int p_Spieler, int p_FigurHash, int p_Feld) {
         //Altes Feld leeren
-        System.out.println("Size: " + getPlayer(p_Spieler).getFigures().size());
-        int i = getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld();
-
         Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][0] = 0;
         Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][1] = 0;
         //Figur Feld Aktualisieren
@@ -62,36 +75,21 @@ public class Spielfeld extends Spiel {
         Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][1] = p_FigurHash;
     }
 
-    private static boolean walk(int p_Spieler, int p_FigurHash, int p_Feld) {
-        //Feld - Homespot
+    private static boolean gohome(int p_Spieler, int p_FigurHash, int p_Feld) {
+
         if (getPlayer(p_Spieler).getFigures().size() != 0) {
             if (getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getRound() && getWin(p_Spieler) < p_Feld) {
-                int position = p_Feld - getWin(p_Spieler);
-                if (getPlayer(p_Spieler).letsgoin(position)) {
-                    Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][0] = 0;
-                    Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][1] = 0;
-                    getPlayer(p_Spieler).getFigures().remove(getPlayer(p_Spieler).getFigurIndexByHash(p_FigurHash));
-                }
+                //To-Do Entsackem
+                //Altes Feld leeren
+                Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][0] = 0;
+                Spielfeld[getPlayer(p_Spieler).getFigurByHash(p_FigurHash).getFeld()][1] = 0;
+                //Figur löschen
+                getPlayer(p_Spieler).getFigures().remove(getPlayer(p_Spieler).getFigurIndexByHash(p_FigurHash));
+                //winspot+1
+                setWinspot(p_Spieler);
                 return true;
-                // }
-                // return false;
             } else {
-                if (Spielfeld[p_Feld][0] == p_Spieler) {
-                    new PlayerOutput().blockedbyown();
-                    return false;
-                } else if (Spielfeld[p_Feld][0] != p_Spieler && Spielfeld[p_Feld][0] != 0) {
-
-                    //Figur schmeißen
-                    int kickout = getPlayer(Spielfeld[p_Feld][0] - 1).getFigurIndexByHash(Spielfeld[p_Feld][1]);
-                    new PlayerOutput().kicked(getPlayer(p_Spieler - 1).getName(), getPlayer(Spielfeld[p_Feld][0] - 1).getName(), getPlayer(p_Spieler - 1).getId(), Spielfeld[p_Feld][0], kickout);
-
-                    getPlayer(Spielfeld[p_Feld][0] - 1).getFigures().remove(kickout); //GEWORFEN!
-                    switchFig(p_Spieler - 1, p_FigurHash, p_Feld);
-                    return true;
-                } else {
-                    switchFig(p_Spieler - 1, p_FigurHash, p_Feld);
-                    return true;
-                }
+                return false;
             }
         }
         return false;
@@ -101,9 +99,9 @@ public class Spielfeld extends Spiel {
         return winspot;
     }
 
-    static void setWinspot(int index, int p_spieler) {
+    private static void setWinspot(int p_spieler) {
         new PlayerOutput().nextinWin(getPlayer(p_spieler).getName());
-        winspot.get(p_spieler).add(index, true);
+        winspot.get(p_spieler).add(true);
         if (winspot.get(p_spieler).size() == 4) {
             new PlayerOutput().won(getPlayer(p_spieler).getName(), getPlayer(p_spieler).getId());
             stopGame();
